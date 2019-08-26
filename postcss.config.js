fdb9d4e2ffc41e2e66df5e4a516c3d3925f9d52d
Rene Dellefont <renedellefont@gmail.com>
2019-08-26 18:31:11 -0400

    Add other postcss plugins

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,16 @@
 const production = !process.env.ROLLUP_WATCH
-//const purgecss = require('@fullhuman/postcss-purgecss')
+const purgecss = require('@fullhuman/postcss-purgecss')
 
 module.exports = {
-  plugins: [require('postcss-import')(), require('tailwindcss')],
+  plugins: [
+    require('postcss-import')(),
+    require('precss')(),
+    require('tailwindcss'),
+    production && require('autoprefixer'),
+    production &&
+      purgecss({
+        content: ['./**/*.html', './**/*.svelte'],
+        defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+      }),
+  ],
 }
