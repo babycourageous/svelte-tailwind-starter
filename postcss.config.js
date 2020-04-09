d1e636c33bde13269b3ffcadd39231aab04800c5
Rene Dellefont <renedellefont@gmail.com>
2020-04-09 17:08:56 -0400

    Update purgecss extractor to include class:

--- postcss.config.js
+++ postcss.config.js
@@ -10,7 +10,25 @@ module.exports = {
     production &&
       purgecss({
         content: ['./**/*.html', './**/*.svelte'],
-        defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+        defaultExtractor: content => {
+          const regExp = new RegExp(/[A-Za-z0-9-_:/]+/g)
+
+          const matchedTokens = []
+
+          let match = regExp.exec(content)
+
+          while (match) {
+            if (match[0].startsWith('class:')) {
+              matchedTokens.push(match[0].substring(6))
+            } else {
+              matchedTokens.push(match[0])
+            }
+
+            match = regExp.exec(content)
+          }
+
+          return matchedTokens
+        },
       }),
     production && require('cssnano'),
   ],
--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,17 @@
 <script>
   export let name
+  export let test = true
 </script>
+
+<main class="test-apply flex flex-col items-center">
+  <h1 class="font-thin text-6xl uppercase" class:text-orange-500={test}>
+    Hello {name}!
+  </h1>
+  <img class="w-2/3 object-cover" src="/rey.jpg" alt="" />
+</main>
 
 <style type="text/postcss">
   .test-apply {
     @apply w-3/5 mx-auto p-4;
   }
 </style>
-
-<main class="test-apply flex flex-col items-center">
-  <h1 class="font-thin text-6xl text-orange-500 uppercase">Hello {name}!</h1>
-  <img class="w-2/3 object-cover" src="/rey.jpg" alt="" />
-</main>
